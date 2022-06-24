829414b3cf163cfbd953d025c6c714d21f24af96
edwardcastle <sir.edwardcastle@gmail.com>
2022-06-23 23:14:18 -0400

    initial commit

---
+++ src/main/java/com/mycompany/test/Client.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.test;
+
+/**
+ *
+ * @author tinycub
+ */
+public class Client {
+    private String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/mycompany/test/Test.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.test;
+
+
+/**
+ *
+ * @author tinycub
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.show();
+    }
+}
